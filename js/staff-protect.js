4a20cb1beb602836dfff7107ffdb5c06e258bd65
kakurega-labo <udon05.0025@gmail.com>
2026-09-11 03:36:30 +0900

    Update staff-protect.js

--- js/staff-protect.js
+++ js/staff-protect.js
@@ -5,10 +5,8 @@
 
   if (!adminKey) {
     adminKey = prompt("パスワードを入力してください");
-    if (adminKey && adminKey.trim() !== "") {
+    if (adminKey) {
       sessionStorage.setItem('staff_pass', adminKey);
-    } else {
-      document.body.innerHTML = '<div style="color: white; text-align: center; margin-top: 50px;"><h2>サーバーエラー。</h2><p>ページを再読み込みしてパスワードを入力してください。</p></div>';
     }
   }
 })();
