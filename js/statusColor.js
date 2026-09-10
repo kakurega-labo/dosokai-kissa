c999651ff55605627a0f7b56957b36c1b33bcf09
kakurega-labo <udon05.0025@gmail.com>
2026-09-10 14:22:18 +0900

    Update statusColor.js

--- js/statusColor.js
+++ js/statusColor.js
@@ -8,13 +8,13 @@
     statusEl.classList.remove('status-green', 'status-yellow', 'status-red');
 
     if (val === 'green') {
-      statusEl.textContent = '空いてる';
+      statusEl.textContent = '混雑なし';
       statusEl.classList.add('status-green');
     } else if (val === 'yellow') {
       statusEl.textContent = 'やや混雑';
       statusEl.classList.add('status-yellow');
     } else if (val === 'red') {
-      statusEl.textContent = '混雑中';
+      statusEl.textContent = '大変混雑';
       statusEl.classList.add('status-red');
     } else {
       statusEl.textContent = '取得エラー';
